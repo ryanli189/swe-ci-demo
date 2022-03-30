d1f6b45c3621f4deaac99d911b6433a4bdb2aa75
Ryan Li <48565540+ryanli189@users.noreply.github.com>
2022-03-30 15:16:12 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -22,23 +22,38 @@ public class FizzBuzz
 	String processNumbers (int[] numList)
 	{
     	String s = "";
-	    if(numList.length == 0){
+	    if(nums.length == 0){
 	            return "Invalid Input";
 	    }
-	    for(int i = 0; i < numList.length; i++){
-	    	if(numList[i] % 15 == 0){
-	    		s += "FizzBuzz";
+	    for(int i = 0; i < nums.length-1; i++){
+	    	if(nums[i] == 0) {
+	    		s += String.valueOf(0) +", ";
+	    	}
+	    	if(nums[i] % 15 == 0){
+	    		s += "FizzBuzz, ";
 	        }
-	        else if(numList[i] % 5 == 0){
-	        	s += "Buzz";
+	        else if(nums[i] % 5 == 0){
+	        	s += "Buzz, ";
 	        }
-	        else if(numList[i] % 3 == 0){
-	        	s += "Fizz";
+	        else if(nums[i] % 3 == 0){
+	        	s += "Fizz, ";
 	        }
 	        else{
-	        	s += String.valueOf(numList[i]);
+	        	s += String.valueOf(nums[i]) + ", ";
 	        }
 	    }
+	    if(nums[nums.length - 1] == 0) {
+	    	s += String.valueOf(0);
+	    } else if(nums[nums.length - 1] % 15 == 0) {
+	    	s += "FizzBuzz";
+	    } else if(nums[nums.length - 1] % 5 == 0) {
+	    	s += "Buzz";
+	    } else if(nums[nums.length - 1] % 3 == 0) {
+	    	s += "Fizz";
+	    } else {
+	    	s += String.valueOf(nums[nums.length -1]);
+	    }
+	    
 	    return s;
 	}
 
