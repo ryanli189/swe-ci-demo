a631fe49c4202d8800f2d7df06777c106f3054a0
Ryan Li <48565540+ryanli189@users.noreply.github.com>
2022-03-30 14:52:03 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,42 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
+    	if (num < 0) {
+				return "Invalid Input";
+			}
+        	if (num % 3 == 0 && num % 5 != 0 ) {
+        		return "Fizz";
+        	}
+        	if (num % 5 == 0 && num % 3 != 0) {
+        		return "Buzz";
+        	}
+        	if (num % 15 == 0 ) {
+        		return "FizzBuzz";
+        	}
+        	return String.valueOf(num);
 	}
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
+    	String s = "";
+	    if(nums.length == 0){
+	            return "Invalid Input";
+	    }
+	    for(int i = 0; i < nums.length; i++){
+	    	if(nums[i] % 15 == 0){
+	    		s += "FizzBuzz";
+	        }
+	        else if(nums[i] % 5 == 0){
+	        	s += "Buzz";
+	        }
+	        else if(nums[i] % 3 == 0){
+	        	s += "Fizz";
+	        }
+	        else{
+	        	s += String.valueOf(nums[i]);
+	        }
+	    }
+	    return s;
 	}
 
 }
